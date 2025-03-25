23bd2bd52cda2d49cb7ddc8ed0092c21cb3d1c2f
MarinKERBORIOU <62393642+sparte1@users.noreply.github.com>
2025-03-25 15:07:56 +0100

    Update midi_parser.h

--- Traitement des fichiers MIDI/Core/Inc/midi_parser.h
+++ Traitement des fichiers MIDI/Core/Inc/midi_parser.h
@@ -12,10 +12,11 @@
 
 
 extern uint32_t tempo_us;
-extern const uint8_t midi_data[];
+extern const uint16_t midi_data[];
 extern const size_t midi_data_length;
 
 int midi_note_to_led(uint8_t note);
-void process_midi(uint8_t *midi_data, size_t length);
+void process_midi(uint16_t *midi_data, size_t length);
 
 #endif /* INC_MIDI_PARSER_H_ */
+
